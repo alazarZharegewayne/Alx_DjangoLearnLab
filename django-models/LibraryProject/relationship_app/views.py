8f1f6785252cc8943d2f85c7fea6997719019793
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-09-07 19:27:13 +0300

    Add Library import to relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book, Library
+from .models import Book, Library  # Added Library import
 
 # Function-based view to list all books
 def list_books(request):
@@ -9,6 +9,6 @@ def list_books(request):
 
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
-    model = Library
+    model = Library  # This requires the Library import
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
