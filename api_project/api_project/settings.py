51d0ea1eb3dd21962797440e5b3d5692b848c768
alazarZharegewayne <alazarharegewaynez@gmail.com>
2025-09-22 22:57:38 +0300

    Add TokenAuthentication to REST framework settings

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework.TokenAuthentication',
     'api',
 ]
 
